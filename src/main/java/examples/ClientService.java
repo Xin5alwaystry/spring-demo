b099e63c04c9c471e590279a203d7d0890eeda98
xin5 <linxinwu_of_xmu@163.com>
2023-01-08 21:06:13 +0800

    Add comment.

--- src/main/java/examples/ClientService.java
+++ src/main/java/examples/ClientService.java
@@ -8,6 +8,7 @@ public class ClientService {
     private static ClientService clientService = new ClientService();
     private ClientService() {}
 
+    //静态工厂方法
     public static ClientService createInstance() {
         return clientService;
     }
